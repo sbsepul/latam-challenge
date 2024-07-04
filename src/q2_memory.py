a4ae97357d3d29fb359e842e90480938639af083
Sebastian Sepulveda <38366209+sbsepul@users.noreply.github.com>
2024-07-04 19:10:18 -0400

    Revert "Q1"

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
 
-
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
     pass
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
 
-
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
     pass
